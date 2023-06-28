6642f6bba3e05fabd36b39e1e3617e90f8938485
DanIngrao <103468143+DanIngrao@users.noreply.github.com>
2023-06-28 20:00:16 -0300

    Update firebase.config.jsx

--- src/services/firebase/firebase.config.jsx
+++ src/services/firebase/firebase.config.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_apikey,
-  authDomain: import.meta.env.VITE_APP_authDomain,
-  projectId: import.meta.env.VITE_APP_projectId,
-  storageBucket: import.meta.env.VITE_APP_torageBucket,
-  messagingSenderId: import.meta.env.VITE_APP_messagingSenderId,
-  appId: import.meta.env.VITE_APP_appId,
-  measurementId: import.meta.env.VITE_APP_measurementId
+  apiKey: process.env.REACT_APP_apikey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_torageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
