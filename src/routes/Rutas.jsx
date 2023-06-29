ed19733cfd14729cc25a9b14e6e07834772efad6
Dan <daningrao99@gmail.com>
2023-06-28 23:53:03 -0300

    adding background

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -7,22 +7,25 @@ import ItemListContainer from '../components/ItemListContainer/ItemListContainer
 import { CartProvider } from '../context/CartContext'
 import Cart from '../components/Cart/Cart'
 import Checkout from '../components/Checkout/Checkout'
+import './Rutas.css'
 
 const Rutas = () => {
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <NavBar/>
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
-            <Route path='/categoria/:categoryId' element={<ItemListContainer/>}/>
-            <Route path='/cart' element={<Cart/>}/>
-            <Route path='/checkout' element={<Checkout/>}/>
-            <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
-        </Routes>
-      </CartProvider>
-    </BrowserRouter>
+    <div className='router'>
+      <BrowserRouter>
+        <CartProvider>
+          <NavBar/>
+          <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
+              <Route path='/categoria/:categoryId' element={<ItemListContainer/>}/>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/checkout' element={<Checkout/>}/>
+              <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
+          </Routes>
+        </CartProvider>
+      </BrowserRouter>
+    </div>  
   )
 }
 
