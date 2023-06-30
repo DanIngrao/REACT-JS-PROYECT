0e98df8dda840f939bf776b9a5d7623cdb638b12
Dan <daningrao99@gmail.com>
2023-06-30 16:59:08 -0300

    css

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -7,7 +7,6 @@ import ItemListContainer from '../components/ItemListContainer/ItemListContainer
 import { CartProvider } from '../context/CartContext'
 import Cart from '../components/Cart/Cart'
 import Checkout from '../components/Checkout/Checkout'
-import './Rutas.css'
 
 const Rutas = () => {
   return (
